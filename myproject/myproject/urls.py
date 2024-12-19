9a2a88b1bc3d7d81de8a43ffd15559d337352bbf
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-12-19 19:41:58 +0200

    add basic styles

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -21,5 +21,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage),
-    path('about/', views.about),
+    path('about/', views.aboutpage),
 ]
--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,12 +1,12 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 def homepage(request):
-  return HttpResponse('<h1>Hello from Home</h1>')
-  # return render(request, 'index.html')
+  # return HttpResponse('<h1>Hello from Home</h1>')
+  return render(request, 'home.html')
 
-def about(request):
-  return HttpResponse('<h1>About Page</h1>')
-  # return render(request, 'about.html')
+def aboutpage(request):
+  # return HttpResponse('<h1>About Page</h1>')
+  return render(request, 'about.html')
