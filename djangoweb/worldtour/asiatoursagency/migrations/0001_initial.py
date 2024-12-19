a03ffcef6bb528868f92414bdec0bcb861bf304e
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-12-19 16:57:03 +0200

    create tours

--- djangoweb/worldtour/asiatoursagency/migrations/0001_initial.py
+++ djangoweb/worldtour/asiatoursagency/migrations/0001_initial.py
@@ -14,10 +14,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Tour',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
                 ('origin_country', models.CharField(max_length=64)),
+                ('destination_country', models.CharField(max_length=64)),
                 ('number_of_nights', models.IntegerField()),
-                ('destination_country', models.IntegerField()),
                 ('price', models.IntegerField()),
             ],
         ),
--- djangoweb/worldtour/asiatoursagency/models.py
+++ djangoweb/worldtour/asiatoursagency/models.py
@@ -9,5 +9,8 @@ class Tour(models.Model):
     origin_country = models.CharField(max_length=64)
     destination_country = models.CharField(max_length=64)
     number_of_nights = models.IntegerField()
-    destination_country = models.IntegerField()
     price = models.IntegerField()
+
+    # This is a string representation of the tours
+    def __str__(self):
+        return (f"ID: {self.id}: From {self.origin_country} To {self.destination_country}, {self.number_of_nights} nights costs $ {self.price}")
