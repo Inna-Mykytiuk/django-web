4d4664db6df397bf9fbe45748b3e77d133b155d9
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-12-21 12:55:11 +0200

    add check exist user

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,35 +1,43 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+
 def register_view(request):
-  if request.method == 'POST':
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      login(request, form.save())
-      # return redirect('posts:list')
-      return redirect('users:login')
-  else:
-    form = UserCreationForm()
-  return render(request, 'users/register.html', {'form': form})
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            login(request, form.save())
+            # return redirect('posts:list')
+            return redirect('users:login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
 
 
-def login_view(request): 
-    if request.method == "POST": 
+def login_view(request):
+    if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
-        if form.is_valid(): 
+        if form.is_valid():
             login(request, form.get_user())
             if 'next' in request.POST:
                 return redirect(request.POST.get('next'))
             else:
                 return redirect("posts:list")
-    else: 
+        else:
+            # Перевіряємо, чи існує користувач
+            username = request.POST.get('username', '')
+            if not User.objects.filter(username=username).exists():
+                return redirect("users:register")
+    else:
         form = AuthenticationForm()
-    return render(request, "users/login.html", { "form": form })
+    return render(request, "users/login.html", {"form": form})
 
 
 def logout_view(request):
-    if request.method == "POST": 
-        logout(request) 
+    if request.method == "POST":
+        logout(request)
         return redirect("posts:list")
